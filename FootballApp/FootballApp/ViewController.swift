66b0fcb9bcfd616340fda86c53afcb40880dc133
유재홍 <ujhong7@gmail.com>
2024-10-04 23:46:16 +0900

    프로젝트 생성

---
+++ FootballApp/FootballApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FootballApp
+//
+//  Created by yujaehong on 10/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
