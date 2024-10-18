26d8a7dafb0889221dfa2ea29bf8dddc912dc172
유재홍 <ujhong7@gmail.com>
2024-10-18 17:29:28 +0900

    GameViewController 화면스와이프 UIPageViewController 추가

--- FootballApp/FootballApp/Feature/Game/GameViewController.swift
+++ FootballApp/FootballApp/Feature/Game/GameViewController.swift
@@ -12,63 +12,46 @@ final class GameViewController: UIViewController {
     // MARK: - Properties
     
     let segmentedControl = UISegmentedControl(items: ["팀순위", "경기결과", "경기예정", "득점순위", "도움순위"])
-    // 자식 뷰 컨트롤러를 위한 변수 추가 ⭐
-    var gameResultVC: GameResultViewController?
-    var upcomingMatchesVC: UpcomingMatchesViewController?
-    var teamRankingVC: TeamRankingViewController?
-    var goalsRankingVC: GoalsRankingViewController?
-    var assistsRankingVC: AssistsRankingViewController?
+    var pageViewController: UIPageViewController!
+    var viewControllers: [UIViewController] = []
+    var currentSegmentIndex: Int = 0
     
     // MARK: - LifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .premierLeaguePurple // white
+        view.backgroundColor = .premierLeaguePurple
         setupNavigationBar()
         setupSegmentedControl()
-        setupInitialView()
+        setupViewControllers()
+        setupPageViewController()
     }
     
     // MARK: - Methods
     
     private func setupNavigationBar() {
         NavigationTitleUtility.setupNavigationTitle(for: self, title: "Premier League")
-        
-        // "pl" 이미지를 UIImage로 생성
         let image = UIImage(named: "pl")
-        
-        // 이미지의 크기를 설정 (필요에 따라 조정)
         let imageView = UIImageView(image: image)
         imageView.contentMode = .scaleAspectFit
         imageView.clipsToBounds = true
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        
-        // 왼쪽 바 버튼 아이템으로 설정
         let imageBarButtonItem = UIBarButtonItem(customView: imageView)
-        
-        // 바 버튼 아이템의 크기를 설정하여 이미지가 잘 보이도록 조정
-        imageBarButtonItem.width = 40 // 원하는 크기로 조정
-        
-        // 네비게이션 아이템에 추가
+        imageBarButtonItem.width = 40
         self.navigationItem.leftBarButtonItem = imageBarButtonItem
         
-        // 이미지 뷰의 제약 조건 설정 (높이 및 너비)
         NSLayoutConstraint.activate([
-            imageView.heightAnchor.constraint(equalToConstant: 30), // 높이 조정
-            imageView.widthAnchor.constraint(equalToConstant: 30)   // 너비 조정
+            imageView.heightAnchor.constraint(equalToConstant: 30),
+            imageView.widthAnchor.constraint(equalToConstant: 30)
         ])
     }
     
     private func setupSegmentedControl() {
         segmentedControl.translatesAutoresizingMaskIntoConstraints = false
-        // 선택되지 않은 세그먼트의 텍스트 색상
-        segmentedControl.setTitleTextAttributes([.foregroundColor: UIColor.gray], for: .normal) // gray
-        // 선택된 세그먼트의 텍스트 색상
+        segmentedControl.setTitleTextAttributes([.foregroundColor: UIColor.gray], for: .normal)
         segmentedControl.setTitleTextAttributes([.foregroundColor: UIColor.premierLeaguePurple, .font: UIFont.boldSystemFont(ofSize: 14)], for: .selected)
-        // 선택되지 않은 세그먼트의 배경색
-        segmentedControl.backgroundColor = UIColor.systemGray6 // systemGray6
-        // 선택된 세그먼트의 배경색
-        segmentedControl.selectedSegmentTintColor = .white // premierLeaguePurple
+        segmentedControl.backgroundColor = UIColor.systemGray6
+        segmentedControl.selectedSegmentTintColor = .white
         
         view.addSubview(segmentedControl)
         
@@ -83,69 +66,76 @@ final class GameViewController: UIViewController {
         segmentedControl.addTarget(self, action: #selector(segmentChanged), for: .valueChanged)
     }
     
-    func setupInitialView() {
-        teamRankingVC = TeamRankingViewController() // 초기 뷰 컨트롤러 인스턴스화 ⭐
-        transition(to: teamRankingVC!) // 초기 뷰 전환 ⭐
+    private func setupViewControllers() {
         let teamRankingVC = TeamRankingViewController()
-    }
-    
-    func transition(to newVC: UIViewController) {
-        for child in children {
-            if child != newVC { // 새 뷰 컨트롤러와 다른 경우에만 제거 ⭐
-                child.willMove(toParent: nil)
-                child.view.removeFromSuperview()
-                child.removeFromParent()
-            }
-        }
+        let gameResultVC = GameResultViewController()
+        let upcomingMatchesVC = UpcomingMatchesViewController()
+        let goalsRankingVC = GoalsRankingViewController()
+        let assistsRankingVC = AssistsRankingViewController()
         
-        if !children.contains(newVC) { // 현재 자식 뷰 컨트롤러에 없으면 추가 ⭐
-            addChild(newVC)
-            newVC.view.translatesAutoresizingMaskIntoConstraints = false
-            view.addSubview(newVC.view)
-            setupConstraints(for: newVC)
-            newVC.didMove(toParent: self)
-        }
+        viewControllers = [teamRankingVC, gameResultVC, upcomingMatchesVC, goalsRankingVC, assistsRankingVC]
     }
     
-    private func setupConstraints(for viewController: UIViewController) {
+    private func setupPageViewController() {
+        pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+        pageViewController.dataSource = self
+        pageViewController.delegate = self
+        
+        // 첫 번째 뷰 컨트롤러 표시
+        pageViewController.setViewControllers([viewControllers[0]], direction: .forward, animated: true, completion: nil)
+        
+        addChild(pageViewController)
+        view.addSubview(pageViewController.view)
+        setupPageViewControllerConstraints()
+        pageViewController.didMove(toParent: self)
+    }
+    
+    private func setupPageViewControllerConstraints() {
+        pageViewController.view.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            viewController.view.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 10),
-            viewController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            viewController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            viewController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            pageViewController.view.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 10),
+            pageViewController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            pageViewController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            pageViewController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
     }
     
     @objc func segmentChanged() {
-        switch segmentedControl.selectedSegmentIndex {
-        case 0:
-            if teamRankingVC == nil { // 인스턴스가 없을 경우 생성 ⭐
-                teamRankingVC = TeamRankingViewController()
-            }
-            transition(to: teamRankingVC!)
-        case 1:
-            if gameResultVC == nil { // 인스턴스가 없을 경우 생성 ⭐
-                gameResultVC = GameResultViewController()
-            }
-            transition(to: gameResultVC!)
-        case 2:
-            if upcomingMatchesVC == nil { // 인스턴스가 없을 경우 생성 ⭐
-                upcomingMatchesVC = UpcomingMatchesViewController()
-            }
-            transition(to: upcomingMatchesVC!)
-        case 3:
-            if goalsRankingVC == nil { // 인스턴스가 없을 경우 생성 ⭐
-                goalsRankingVC = GoalsRankingViewController()
-            }
-            transition(to: goalsRankingVC!)
-        case 4:
-            if assistsRankingVC == nil { // 인스턴스가 없을 경우 생성 ⭐
-                assistsRankingVC = AssistsRankingViewController()
-            }
-            transition(to: assistsRankingVC!)
-        default:
-            break
+        let newSegmentIndex = segmentedControl.selectedSegmentIndex
+        let direction: UIPageViewController.NavigationDirection = newSegmentIndex > currentSegmentIndex ? .forward : .reverse
+        
+        pageViewController.setViewControllers([viewControllers[newSegmentIndex]], direction: direction, animated: true, completion: nil)
+        currentSegmentIndex = newSegmentIndex
+    }
+}
+
+// MARK: - UIPageViewControllerDataSource
+
+extension GameViewController: UIPageViewControllerDataSource {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let index = viewControllers.firstIndex(of: viewController), index > 0 else {
+            return nil
         }
+        return viewControllers[index - 1]
     }
     
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let index = viewControllers.firstIndex(of: viewController), index < viewControllers.count - 1 else {
+            return nil
+        }
+        return viewControllers[index + 1]
+    }
+}
+
+// MARK: - UIPageViewControllerDelegate
+
+extension GameViewController: UIPageViewControllerDelegate {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        if completed, let visibleViewController = pageViewController.viewControllers?.first, let index = viewControllers.firstIndex(of: visibleViewController) {
+            segmentedControl.selectedSegmentIndex = index
+            currentSegmentIndex = index
+        }
+    }
 }
