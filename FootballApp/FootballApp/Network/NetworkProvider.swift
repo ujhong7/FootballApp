f9d4e35e03f300e8dfe408c0e0108de7b4e54734
유재홍 <ujhong7@gmail.com>
2024-10-23 23:28:48 +0900

    에러처리 추가

--- FootballApp/FootballApp/Network/NetworkProvider.swift
+++ FootballApp/FootballApp/Network/NetworkProvider.swift
@@ -11,7 +11,10 @@ enum NetworkError: Error {
     case invalidURL
     case noData
     case decodingError
+    case httpError(statusCode: Int)
+    case unknownError
 }
+
 
 final class NetworkProvider {
     
@@ -46,9 +49,23 @@ final class NetworkProvider {
                 return
             }
             
+            // HTTP 응답 상태 코드 확인
+            if let httpResponse = response as? HTTPURLResponse {
+                guard (200...299).contains(httpResponse.statusCode) else {
+                    completion(.failure(NetworkError.httpError(statusCode: httpResponse.statusCode)))
+                    return
+                }
+            }
+            
             guard let data = data else {
                 completion(.failure(NetworkError.noData))
                 return
+            }
+            
+            // 디버그용으로 받은 JSON 데이터 출력
+            if let jsonString = String(data: data, encoding: .utf8) {
+                //dump("Received JSON: \(jsonString)")
+                print("jsonString: \(jsonString)")
             }
             
             do {
